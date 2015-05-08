aee2e62f6ac980dcd04d09639f323c1c7ba380bc
heartstchr <heartstchr@live.com>
2015-05-08 11:02:16 +0530

    mongodb and post in admin associated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# 
+from mongoengine import *
+from convergence.settings import DBNAME
+
+connect(DBNAME)
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    content = StringField(max_length=500, required=True)
+    last_update = DateTimeField(required=True)
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models import Post
+import datetime
 
 # Create your views here.
 
 def home(request):
+       posts = Post.objects 
        context = {'title':'Convergence'}
-       return render_to_response('index.html', context, context_instance=RequestContext(request))
+       return render_to_response('index.html', {'Posts': posts}, context_instance=RequestContext(request))
 
 def post(request):
        context = {'title':'Convergence | Post'}
        return render_to_response('post.html', context, context_instance=RequestContext(request))
 
 def admin(request):
+    if request.method == 'POST':
+       # save new post
+       title = request.POST['title']
+       content = request.POST['content']
+       #print(request.POST[all])
+       post = Post(title=title)
+       post.last_update = datetime.datetime.now() 
+       post.content = content
+       post.save()
        context = {'title':'Convergence | Admin'}
-       return render_to_response('admin/index.html', context, context_instance=RequestContext(request))
+    return render_to_response('admin/index.html',context_instance=RequestContext(request))
