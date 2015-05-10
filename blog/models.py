d40c59bf7db3d62eae01d5c382d29a4691068cf0
Kumar Anirudha <anirudhastark@yahoo.com>
2015-05-10 14:19:12 +0530

    dbname deprecated old format

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,6 @@ from django.db import models
 # Create your models here.
 
 from mongoengine import *
-from convergence.settings import DBNAME
-
-connect(DBNAME)
-
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
--- convergence/settings.py
+++ convergence/settings.py
@@ -27,8 +27,6 @@ DATABASES = {
     }
 }
 
-DBNAME = 'convergence_blog'
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
