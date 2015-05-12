e93207a85f0160b5986acd1cb78392afe16861e9
heartstchr <heartstchr@live.com>
2015-05-13 00:50:01 +0530

    database name changed , email validation in views.py , admin/home replaced with admin only url fixed

--- convergence/urls.py
+++ convergence/urls.py
@@ -12,11 +12,17 @@ urlpatterns = patterns('',
 	url(r'^article/(?P<postid>\w+)/', 'blog.views.article', name='article'),
 
 	# Admin Pages
-	url(r'^admin/home', 'blog.views.admin', name='admin'),
+	url(r'^admin/$', 'blog.views.admin', name='admin'),
 	url(r'^admin/posts/edit', 'blog.views.admin_edit_posts', name='edit_posts'),
 
 	# login/register Pages
 	url(r'^user/register', 'blog.views.user_register', name='user.register'),
+
+	# login/login Pages
+	#url(r'^user/login', 'blog.views.user_login', name='user.login'),
+
+	# login/forget password Pages
+	#url(r'^user/forgetpasword', 'blog.views.user_forgetpasword', name='user.forgetpasword'),
 
 	# User Pages
 	url(r'^user/(?P<userid>\w+)/', 'blog.views.user_profile', name='user.profile'),
