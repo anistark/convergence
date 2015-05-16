9ffe6e19fc6873d66bd0cc4399668bb5fa44289a
Kumar Anirudha <anirudhastark@yahoo.com>
2015-05-17 01:30:51 +0530

    forms trial bootstrapping done. Image field issue.

---
+++ convergence/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	# url(r'^$', 'convergence.views.home', name='home'),
+	# url(r'^convergence/', include('convergence.foo.urls')),
+
+	url(r'^$', 'blog.views.home', name='home'),
+	url(r'^article/(?P<postid>\w+)/', 'blog.views.article', name='article'),
+
+	# Admin Pages
+	url(r'^admin/$', 'blog.views.admin', name='admin'),
+	url(r'^admin/posts/edit', 'blog.views.admin_edit_posts', name='admin.edit_posts'),
+
+	# login/register Pages
+	url(r'^user/register', 'blog.views.user_register', name='user.register'),
+
+	# login/login Pages
+	#url(r'^user/login', 'blog.views.user_login', name='user.login'),
+
+	# login/forget password Pages
+	#url(r'^user/forgetpasword', 'blog.views.user_forgetpasword', name='user.forgetpasword'),
+
+	# User Pages
+	url(r'^user/(?P<userid>\w+)/', 'blog.views.user_profile', name='user.profile'),
+
+)
